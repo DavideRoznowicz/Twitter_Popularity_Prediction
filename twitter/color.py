2f4252f569279dcff4b94ef1684fb50e9466ce1d
Davide <davide.roznowicz@gmail.com>
2021-04-03 18:26:27 +0200

    update

--- twitter/color.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import cv2 as cv
-from matplotlib import pyplot as plt
-img = cv.imread('129168397_714202876183863_4207819796983216597_n.jpg',0)
-hist_img = cv.calcHist([img],[0],None,[256],[0,256])
-
